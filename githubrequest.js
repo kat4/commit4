ff7722529b4f6fc17dcfaa0f0665db2e282cd0e1
mantagen <mathsgregory@gmail.com>
2015-10-28 15:43:17 +0000

    Getting commit file details from GH

--- githubrequest.js
+++ githubrequest.js
@@ -3,32 +3,46 @@ var qs = require('querystring');
 var env = require('env2')('./config.env');
 
 var credentials = {
-  client_id : process.env.CLIENT_ID,
-  client_secret : process.env.CLIENT_SECRET
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET
 };
 
 var options = {
-  host:'api.github.com',
-  method:'GET',
-  path:'/repos/kat4/commit4/commits?'+qs.stringify(credentials),
-  headers:{
-    'User-agent':'commit4'
+  host: 'api.github.com',
+  method: 'GET',
+  path: '/repos/kat4/commit4/commits?' + qs.stringify(credentials),
+  headers: {
+    'User-agent': 'commit4'
   }
 };
 
-var requestCommits = https.request(options, function(res) {
-  parseResponse(res, function(body) {
-    console.log(body);
+var requestCommitHistory = https.request(options, function(res) {
+
+  parseBody(res, function(body) {
+
+    JSON.parse(body).forEach(function(commit) {
+
+      console.log(commit.sha);
+      options.path = '/repos/kat4/commit4/commits/'+commit.sha+'?' + qs.stringify(credentials);
+      var getCommitDetails = https.request(options, function(res) {
+        parseBody(res,function(body){
+          console.log(JSON.parse(body).files);
+        });
+      });
+      getCommitDetails.end();
+
+
+    });
   });
 });
 
-requestCommits.on('error', function(e) {
+requestCommitHistory.on('error', function(e) {
   console.log('problem with request: ' + e.message);
 });
 
-requestCommits.end();
+requestCommitHistory.end();
 
-function parseResponse(object, callback) {
+function parseBody(object, callback) {
   var body = '';
   object.setEncoding('utf8');
   object.on('data', function(chunk) {
