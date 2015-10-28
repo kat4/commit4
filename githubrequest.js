2b72f05b3b6021fa6fd88c0459bbfb79c6d47804
mantagen <mathsgregory@gmail.com>
2015-10-28 16:22:21 +0000

    Creating a commitHistory obect

--- githubrequest.js
+++ githubrequest.js
@@ -17,16 +17,31 @@ var options = {
 };
 
 var requestCommitHistory = https.request(options, function(res) {
-
+  var commitHistory = {};
   parseBody(res, function(body) {
 
     JSON.parse(body).forEach(function(commit) {
-
+      //console.log(commit);
       console.log(commit.sha);
+      console.log(commit.committer.login);
+      console.log(commit.committer.avatar_url);
+      console.log(commit.commit.committer.date);
+      console.log(commit.committer.id);
+
+      commitHistory[commit.commit.committer.date] = {
+        author:{
+          username: commit.committer.login,
+          avatar: commit.committer.avatar_url
+        },
+        files:{
+
+        }
+      };
+      console.log(commitHistory);
       options.path = '/repos/kat4/commit4/commits/'+commit.sha+'?' + qs.stringify(credentials);
       var getCommitDetails = https.request(options, function(res) {
         parseBody(res,function(body){
-          console.log(JSON.parse(body).files);
+          //console.log(JSON.parse(body).files);
         });
       });
       getCommitDetails.end();
