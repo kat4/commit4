1bfc4cbee5ea6ea9cdeb1ba7dca8e9400aec6979
katkelemen <kelemenkata@gmail.com>
2015-10-29 14:15:17 +0000

    first tests in progress

--- tests/backendtest.js
+++ tests/backendtest.js
@@ -1,9 +1,31 @@
 var shot = require('shot');
 var test = require('tape');
 var handler = require("../handlers.js");
+var router = require("../router.js");
+var server = require("../server.js").server;
 
 
 test('basic mathematics', function(t){
     t.equal(1+1,2, 'wicked, you got this right!!!~!~!');
     t.end();
+});
+
+// test('"/" url should use handlers.home and return index page', function(t){
+//     shot.inject(handler, {
+//         method: "GET",
+//         url: "/"
+//     }, function(res) {
+//         t.equal(res.statusCode,200);
+//         t.end();
+//     });
+// });
+
+test('unhandled url should return resource not found', function(t){
+    shot.inject(server, {
+        method: "GET",
+        url: "/ducktales/"
+    }, function(res) {
+        t.equal(res.payload, 'Resource not found!');
+        t.end();
+    });
 });
